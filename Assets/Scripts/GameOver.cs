3f380734528dd04b88284305b4895e8b152443a0
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-06-21 18:48:16 -0300

    Adicionando tela de game over

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject GameOverCanvas;
+    public Snake snakeController; // Importa a Snake
+    private bool isGameOver = false;
+
+    // Garante que a tela de Game Over comece inativa
+    private void Start()
+    {
+        GameOverCanvas.SetActive(false);
+    }
+
+    // Ativa a tela de game over e pausa o jogo
+    public void EndGame()
+    {
+        if (!isGameOver && snakeController != null)
+        {
+            Debug.Log("Game Over Triggered");
+
+            // Desabilita o colisor da Snake para evitar colisões fantasmas
+            snakeController.snakeCollider.enabled = false;
+            Debug.Log("Snake Collider disabled");
+
+            GameOverCanvas.SetActive(true); // Activate the Game Over Canvas
+            Time.timeScale = 0f; // Pause the game
+            isGameOver = true;
+        }
+    }
+
+    // Reinicia o jogo 
+    public void Restart()
+    {
+        Debug.Log("Restarting the game");
+        Time.timeScale = 1f; // Despausar o jogo 
+        GameOverCanvas.SetActive(false); // Desativar a tela de Game Over 
+        StartCoroutine(RestartCoroutine());
+    }
+
+    // Coroutine para reiniciar o jogo
+    private IEnumerator RestartCoroutine()
+    {
+        snakeController.ResetState(); // Reiniciar o estado da Snake 
+
+        yield return new WaitForSeconds(0.1f); // Esperar para evitar colisões fantasmas 
+        snakeController.EnableCollider(); // Reabilitar o colisor da Snake
+        isGameOver = false; // Indica que o jogo não foi terminado
+        Debug.Log("Game restarted");
+    }
+}
